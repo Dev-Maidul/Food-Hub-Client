1ef61ea74599d0ffeb0b7fae981e1f1db676b30a
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-03-18 14:37:22 +0800

    my previous repo is not working and can not push code I have tried many times thats why creating this new repo

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,53 @@
+import { categoryActions } from "@/actions/categories";
+import { getPopularMeals } from "@/actions/reviews";
+import { CategorySlider } from "@/components/modules/Home/CategorySlider";
+import HeroSection from "@/components/modules/Home/Hero";
+import { HowItWorks } from "@/components/modules/Home/HowItWorks";
+import { OrderTracking } from "@/components/modules/Home/OrderTracking";
+import { PopularMeals } from "@/components/modules/Home/PopularMeals";
+import {
+  CategorySliderSkeleton,
+  PopularMealsSkeleton,
+} from "@/helper/skelitonLoader";
+import { Suspense } from "react";
+export const dynamic = "force-dynamic";
+
+async function getHomeData() {
+  try {
+    const [categoriesData, popularMealsData] = await Promise.all([
+      categoryActions(),
+      getPopularMeals(),
+    ]);
+    return {
+      categories: categoriesData?.data?.data || [],
+      meals: popularMealsData?.data?.data || [],
+      error: null,
+    };
+  } catch (error) {
+    console.error("Error fetching home data:", error);
+    return {
+      categories: [],
+      meals: [],
+      error: "Failed to load data",
+    };
+  }
+}
+
+export default async function Home() {
+  const { categories, meals, error } = await getHomeData();
+
+  return (
+    <>
+      <HeroSection />
+      <Suspense fallback={<CategorySliderSkeleton />}>
+        <CategorySlider categories={categories} />
+      </Suspense>
+      <Suspense fallback={<PopularMealsSkeleton />}>
+        <PopularMeals meals={meals?.data} />
+      </Suspense>
+      <HowItWorks />
+      <OrderTracking />
+    </>
+  );
+}
+// export const revalidate = 300;
